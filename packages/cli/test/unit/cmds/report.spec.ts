388c046e89276b68439f1fe7b8c522b191bbccc4
Douglas Duteil <douglasduteil@gmail.com>
2017-06-16 17:11:06 +0200

    text(cli): update report cmd test

--- packages/cli/test/unit/cmds/report.spec.ts
+++ packages/cli/test/unit/cmds/report.spec.ts
@@ -4,25 +4,25 @@
 import { test } from 'ava';
 import { sandbox } from 'sinon';
 
-import { Reporter } from '@perfyjs/core';
-
 import { handlerFactory, IHandlerFactoryContext } from '../../../src/cmds/report';
 
 //
 
 test.beforeEach((t) => {
   t.context = {
-    reporter: Reporter.prototype,
+    copy: () => {},
+    database: () => {},
+    resolve: () => {},
     log: {silly() {}, error() {}}
   } as any as Partial<IHandlerFactoryContext>;
 });
 
 test('should report ', async (t) => {
   const sandpit = sandbox.create();
-  const mocks = sandpit.mock(t.context.reporter);
+  const mocks = sandpit.mock(t.context);
 
-  mocks.expects('resolve').once().withArgs('web');
-  mocks.expects('report').once().withArgs({}).resolves();
+  mocks.expects('resolve').once().withArgs('web').returns('path/to/web_report');
+  mocks.expects('copy').once().withArgs('path/to/web_report', 'perfy_report').resolves();
 
   const handler = handlerFactory(t.context);
 
